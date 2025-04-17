c48eaa8175af2eb9dbd57a2662ecf181e97cacdb
Vanderlei Alvarenga Jr <jubalupe@gmail.com>
2025-04-17 14:39:08 -0300

    Adicionados os arquivos iniciais do projeto SpringBoot.

---
+++ springboot-inicial/src/main/java/com/vanderleialvarengajr/springboot_inicial/SpringbootInicialApplication.java
@@ -0,0 +1,13 @@
+package com.vanderleialvarengajr.springboot_inicial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootInicialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootInicialApplication.class, args);
+	}
+
+}
